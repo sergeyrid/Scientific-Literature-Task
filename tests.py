5460085ed6c7e6dda7854db3a755ac3a9ea11ec3
sergeyrid <sergeyrid@yandex.ru>
2022-02-15 16:56:39 +0300

    Add test1

---
+++ tests.py
@@ -0,0 +1,32 @@
+from main import main
+from os.path import isfile
+
+all_path = 'test_files/'
+in_default_name = 'input.txt'
+out_default_name = 'output.txt'
+exp_default_name = 'expected.txt'
+
+
+def check(exp_name: str, out_name: str) -> None:
+    assert isfile(exp_name)
+    assert isfile(out_name)
+    expected = []
+    with open(exp_name, 'r') as exp_file:
+        for line in exp_file.readlines():
+            if line.isnumeric():
+                expected.append(int(line))
+    output = []
+    with open(out_name, 'r') as out_file:
+        for line in out_file.readlines():
+            if line.isnumeric():
+                output.append(int(line))
+    assert expected == output
+
+
+class TestDummy:
+    dummy_path = all_path + 'dummy_tests/'
+
+    def test1(self) -> None:
+        test1_path = self.dummy_path + 'test1/'
+        main(test1_path + in_default_name, test1_path + out_default_name)
+        check(test1_path + exp_default_name, test1_path + out_default_name)
